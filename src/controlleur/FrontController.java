8921ccff20510cba70bde767acc3e6361223fc0b
Pokaneliot <161971448+Pokaneliot@users.noreply.github.com>
2024-06-11 09:36:59 +0300

    sprint5: gestion d'exception

--- src/controlleur/FrontController.java
+++ src/controlleur/FrontController.java
@@ -1,10 +1,10 @@
 package mg.pokaneliot.controlleur;
 
 import java.io.*;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
 import java.io.PrintWriter;
 import java.util.Map;
 import mg.pokaneliot.util.ModelAndView;
@@ -13,10 +13,17 @@ import mg.pokaneliot.util.Scanner;
 
 public class FrontController extends HttpServlet {
 	Map<String,Mapping> urlMap;
+    String message;
 
     public void init() throws ServletException{
-        String controllPackage = this.getInitParameter("controllerRepository");
-        this.urlMap = Scanner.scanCurrentProjet(controllPackage);
+        visited = false;
+        String controllPackage = this.getInitParameter("controllPackage");
+        try{
+            String controllPackage = this.getInitParameter("controllerRepository");
+            this.urlMap = Scanner.scanCurrentProjet(controllPackage);
+        } catch(Exception e){
+            message = e.getMessage();
+        }
     }
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	processRequest(request,response);	
@@ -24,51 +31,77 @@ public class FrontController extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	processRequest(request,response);
     }
-    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	PrintWriter out = response.getWriter();
-        String url=request.getRequestURL().toString();
-        int check=0;
-    	out.print("vous êtes dans "+url+"\n");
-        for(String key : this.urlMap.keySet()){
-            int len=key.length()-1;
-            int urlLen=url.length()-1;
-            String urlkey=url.substring(urlLen-len,urlLen);
-            if (key.compare(urlkey)==0) {
-                Mapping map= this.urlMap.get(key);
-                out.print("L'url : "+ urlkey +" est associé à la methode "+map.getMethodName()+"dans la class "+map.getClassName()+"\n");
-                out.print("l'output : "+executeMethode(map,out,request,response));
-                check=1;
-            }
+    protected void processRequest(HttpServletRequest req,HttpServletResponse rep) throws ServletException,IOException{
+        PrintWriter out = rep.getWriter();
+        if(message != null){
+            out.println(message);
         }
-        if (check==0){
-                out.print("L'url : "+ urlkey +" n'est pas associé à une methode dans une classe annotée controller \n");
-        }
-    }
-    public static void executeMethode(Mapping map,PrintWriter out,HttpServletRequest req, HttpServletResponse res) throws Exception{
-        String clname=map.getClassName();
-        String mthname=map.getMethodName();
-        Class<?> cl=Class.forName(clname);
-        Method mth=cl.getDeclaredMethod(mthname);
-        Object classrep=cl.getDeclaredConstructor().newInstance();
-        Object obj=mth.invoke(classrep);
-         if(obj instanceof String){
-            out.print((String)obj);
-        }
-        else if(obj instanceof ModelAndView){
-            ModelAndView modelV = (ModelAndView)obj;
-            Map<String,Object>map = modelV.getData();
-            RequestDispatcher dispat = req.getRequestDispatcher(modelV.getViewUrl());
-            for(Map.Entry<String,Object> entry : map.entrySet()){
-                String dataName = (String)entry.getKey();
-                Object data = entry.getValue();
-                req.setAttribute(dataName,data);
-            }
-            dispat.forward(req,res);
-        } 
         else{
-            String resultType = obj.getClass().getSimpleName();
-            throw new Exception("Execution impossible pour le type de retour : "+resultType);
-        }
+            String url = req.getRequestURL().toString();
+            String[] urlParts = url.split("/");
+            int i = 1;
+            String urlTarget = "/"+urlParts[urlParts.length - 1];
+            boolean ver = false;
+            else{
+                out.print("Vous avez entrez cet url :"+url+"\n");
+                while(i < urlParts.length){
+                    if(this.urlMap.containsKey(urlTarget)){
+                        Mapping mapping = this.urlMap.get(urlTarget);
+                        out.println(String.format("La methode %s de la classe %s a ete appelee\n", mapping.getMethodName(),mapping.getClassName()));
+                        try{
+                            Object obj = executeMethode(mapping);
+                            if(obj instanceof String){
+                                out.print((String)obj);
+                            }
+                            else if(obj instanceof ModelAndView){
+                                ModelAndView modelV = (ModelAndView)obj;
+                                Map<String,Object>map = modelV.getData();
+                                if(!this.urlView.contains(modelV.getViewUrl())){
+                                    out.print("Erreur 404: la page "+modelV.getViewUrl()+" n'existe pas");
+                                }
+                                else{
+                                    RequestDispatcher dispat = req.getRequestDispatcher(modelV.getViewUrl());
+                                    for(Map.Entry<String,Object> entry : map.entrySet()){
+                                        String dataName = (String)entry.getKey();
+                                        Object data = entry.getValue();
+                                        req.setAttribute(dataName,data);
+                                    }
+                                    dispat.forward(req,rep);
+                                }
+                            } 
+                            else{
+                                String resultType = obj.getClass().getSimpleName();
+                                out.print("Execution impossible pour le type de retour : "+resultType);
+                            }
 
+                        } catch(Exception e){
+                            out.println(e.getMessage());
+                        }
+                        ver = true;
+                        break;
+                    }
+                    else{
+                        urlTarget = "/"+urlParts[urlParts.length - (i + 1)]+urlTarget;
+                    }
+                    i++;
+                }
+                if(!ver){
+                    out.print(String.format("Erreur 404 : Aucune methode n'est associe à cet url : %s\n", urlTarget));
+                }         
+                
+            }
+        }   
+        
+        
+    }
+    public static Object executeMethode(Mapping target) throws Exception{
+        String className = target.getClassName();
+        String methodeName = target.getMethodName();
+        Class<?>cl = Class.forName(className);
+        Method me = cl.getDeclaredMethod(methodeName);
+        Object instance = cl.getDeclaredConstructor().newInstance();
+        Object obj = me.invoke(instance);
+        return obj;
+        
     }
 }
